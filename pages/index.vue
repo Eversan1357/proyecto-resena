416a82cb58905f3b6148c7b318ffb5005b904075
Eversan1357 <es.basto_2019-1@corhuila.edu.co>
2021-11-29 15:07:43 -0500

    pantalla de inicio

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,64 @@
-<template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Holaaaaaaaaaaaaaaa to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>holaaaa is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+<template lange="html">
+
+
+  <v-card
+    height="630"
+    width="2200"
+
+    class="center"
+  >
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Reseñas
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Opciones
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'Buscar', icon: 'mdi-account' },
+          { title: 'Empresa 1', icon: 'mdi-domain' },
+          { title: 'Empresa 2', icon: 'mdi-domain' },
+          { title: 'Empresa 3', icon: 'mdi-domain' },
+          { title: 'Empresa 4', icon: 'mdi-domain' },
+          { title: 'Empresa 5', icon: 'mdi-domain' },
+          { title: 'Empresa 6', icon: 'mdi-domain' },
+          { title: 'Agregar Empresa', icon: 'mdi-plus' }
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
